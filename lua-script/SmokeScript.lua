1270c8c99ff844d23d2554229d77f91cc1ccb601
NOAAWeatherRadioS <89881391+NOAAWeatherRadioS@users.noreply.github.com>
2021-08-31 23:45:26 -0500

    Create SmokeScript.lua

---
+++ lua-script/SmokeScript.lua
@@ -0,0 +1,8 @@
+local part = Instance.new("Part", game.Workspace)
+local smoke = Instance.new("Smoke", part)
+
+part.Position = Vector3.new(-65.307, 10.326, -180.467)
+part.Anchored = true
+smoke.Size = 5
+smoke.RiseVelocity = 2
+part.Transparency = 1
