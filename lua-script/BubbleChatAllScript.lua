bb89fa634f107d35c804d6ca8427b50d092352ca
NOAAWeatherRadioS <89881391+NOAAWeatherRadioS@users.noreply.github.com>
2022-05-01 17:35:01 -0500

    Update BubbleChatAllScript.lua

--- lua-script/BubbleChatAllScript.lua
+++ lua-script/BubbleChatAllScript.lua
@@ -7,7 +7,7 @@ local Chat = game:GetService("Chat")
 -- Main
 
 for _, plr in next, game.Players:GetPlayers() do --Goes thru all the players in the game
-	local char = plr.Character;
+	local char = plr.Character or plr.CharacterAdded:Wait()
 	local humanoid = char:FindFirstChild("Humanoid");
 Chat:Chat(char, "Type anything you want...", "White")
 end
