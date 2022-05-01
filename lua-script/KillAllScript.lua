39c83263aa6bac5fa043119678e1e3b14247639c
NOAAWeatherRadioS <89881391+NOAAWeatherRadioS@users.noreply.github.com>
2022-05-01 17:36:59 -0500

    Update KillAllScript.lua

--- lua-script/KillAllScript.lua
+++ lua-script/KillAllScript.lua
@@ -4,8 +4,8 @@
 -- Main
 
 for _, plr in next, game.Players:GetPlayers() do --Goes thru all the players in the game
-	local char = plr.Character;
-	local humanoid = char:FindFirstChild("Humanoid");
+	local char = plr.Character or plr.CharacterAdded:Wait()
+	local humanoid = char:FindFirstChild("Humanoid")
 humanoid.Health = 0
 
 end
