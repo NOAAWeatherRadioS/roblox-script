8a7e8a498b64275c2ec5fce7fdedb477730acb1b
NOAAWeatherRadioS <89881391+NOAAWeatherRadioS@users.noreply.github.com>
2022-07-15 00:20:26 -0500

    Update PlaySoundV2.lua

--- lua-script/PlaySoundV2.lua
+++ lua-script/PlaySoundV2.lua
@@ -11,3 +11,19 @@ sound.Volume = 1
 sound.PlaybackSpeed = 1
 sound.Looped = true
 sound:Play()
+
+-- wanna play when removed
+-- Things thats required
+
+local SoundID = [TYPE YOUR SOUND ID HERE]
+
+local sound = Instance.new("Sound", game.Workspace)
+
+sound.SoundId = "rbxassetid://"..SoundID
+
+-- Things that you can change
+sound.Volume = 1
+sound.PlaybackSpeed = 1
+sound.Looped = true
+sound.PlayOnRemove = true
+sound:Destroy()
